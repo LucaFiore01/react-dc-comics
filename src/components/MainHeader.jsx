7b00d482601d65dbce6ab0262eab13b5237b7dae
Luca Fiore <lf44551@gmail.com>
2026-01-19 14:09:51 +0100

    Sostituisci il menu di navigazione statico con il componente NavBar dinamico

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,23 +1,12 @@
+import NavBar from "./NavBar"
+
 const MainHeader = () => {
     return (
         <header>
             <figure>
                 <img src="/src/assets/img/dc-logo.png" alt="Logo DC Comics"></img>
             </figure>
-            <nav>
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Collectibles</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Shop</a></li>
-                </ul>
-            </nav>
+            <NavBar />
         </header>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,27 @@ const NavBar = () => {
         { id: 8, text: 'Fans', url: '#', current: false },
         { id: 9, text: 'News', url: '#', current: false },
         { id: 10, text: 'Shop', url: '#', current: false },
-    ]
+    ];
+
+
+    return (
+        <nav>
+            <ul>
+                {links.map((link) => {
+                    return (
+                        <li key={link.id}>
+                            <a href={link.url}>
+                                {link.text}
+                            </a>
+
+                        </li>
+                    )
+                })}
+
+            </ul>
+        </nav>
+    )
 
 }
+
+export default NavBar 
